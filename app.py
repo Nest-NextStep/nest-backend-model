686f86132e15bf705a011fa60524abea94caf1f7
Al-Ferro Yudisthira Putra <alferroputra2003@gmail.com>
2024-06-18 21:39:27 +0700

    update model and preprocessing

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ import numpy as np
 import pandas as pd
 from flask import Flask, jsonify, request
 from tensorflow.keras.models import load_model
+import joblib
 
 app = Flask(__name__)
-model = load_model("model_2.h5", compile=False)
+model = load_model("my_model2.h5", compile=False)
+scaler = joblib.load('scaler.joblib')
 
 labels = {
     0: 'Administrasi Bisnis',
@@ -66,7 +68,12 @@ I_features = ['I1', 'I2', 'I4', 'I5', 'I7', 'I8']
 A_features = ['A2', 'A3', 'A4', 'A5', 'A6', 'A8']
 S_features = ['S1', 'S3', 'S5', 'S6', 'S7', 'S8']
 E_features = ['E1', 'E3', 'E4', 'E5', 'E7', 'E8']
-C_features = ['C1','C2', 'C3', 'C5', 'C7', 'C8']
+C_features = ['C2', 'C3', 'C5','C6', 'C7', 'C8']
+TIPI_features = ['TIPI1', 'TIPI2', 'TIPI3', 'TIPI4',
+                 'TIPI5', 'TIPI6', 'TIPI7', 'TIPI8', 'TIPI9', 'TIPI10']
+VCL_features = ['VCL1', 'VCL2', 'VCL3', 'VCL4', 'VCL5', 'VCL6',
+                'VCL10', 'VCL11', 'VCL12', 'VCL13', 'VCL14', 'VCL15']
+
 
 
 @app.route("/")
@@ -86,11 +93,8 @@ def predict():
         try:
             data = request.json
             # Ensure all required features are in the input data
-            required_features = R_features + I_features + A_features + S_features + E_features + C_features + [
-                'TIPI1', 'TIPI2', 'TIPI3', 'TIPI4', 'TIPI5', 'TIPI6', 'TIPI7', 'TIPI8', 'TIPI9', 'TIPI10',
-                'VCL1', 'VCL2', 'VCL3', 'VCL4', 'VCL5', 'VCL6', 'VCL10', 'VCL11', 'VCL12', 'VCL13', 'VCL14', 'VCL15',
-                'education', 'gender', 'engnat', 'religion', 'voted'
-            ]
+            required_features = R_features + I_features + A_features + \
+                S_features + E_features + C_features + TIPI_features + VCL_features
 
             for feature in required_features:
                 if feature not in data:
@@ -106,33 +110,42 @@ def predict():
             df = pd.DataFrame([data])
 
             # Sum the feature groups
-            df['R'] = df[R_features].sum(axis=1)
-            df['I'] = df[I_features].sum(axis=1)
-            df['A'] = df[A_features].sum(axis=1)
-            df['S'] = df[S_features].sum(axis=1)
-            df['E'] = df[E_features].sum(axis=1)
-            df['C'] = df[C_features].sum(axis=1)
+            df['R'] = df[R_features].sum(axis=1)/6
+            df['I'] = df[I_features].sum(axis=1)/6
+            df['A'] = df[A_features].sum(axis=1)/6
+            df['S'] = df[S_features].sum(axis=1)/6
+            df['E'] = df[E_features].sum(axis=1)/6
+            df['C'] = df[C_features].sum(axis=1)/6
+            df['TIPI'] = df[TIPI_features].sum(axis=1)
+            df['VCL'] = df[VCL_features].sum(axis=1)
 
             # Select the final 37 features
-            final_features = ['R', 'I', 'A', 'S', 'E', 'C'] + [
-                'TIPI1', 'TIPI2', 'TIPI3', 'TIPI4', 'TIPI5', 'TIPI6', 'TIPI7', 'TIPI8', 'TIPI9', 'TIPI10',
-                'VCL1', 'VCL2', 'VCL3', 'VCL4', 'VCL5', 'VCL6', 'VCL10', 'VCL11', 'VCL12', 'VCL13', 'VCL14', 'VCL15',
-                'education', 'gender', 'engnat', 'religion', 'voted'
-            ]
+            final_features = ['R', 'I', 'A', 'S', 'E', 'C', 'VCL', 'TIPI']
             input_data = df[final_features].values
+            input_data_scaled = scaler.transform(input_data)
+            print(input_data)
 
+            print(input_data_scaled)
             # Predict using the model
-            prediction = model.predict(input_data)
-            y_pred = np.argmax(prediction, axis=1)
+            prediction = model.predict(input_data_scaled)
+            print("Prediction probabilities:", prediction)
 
+            # Get the index of the maximum value along axis 1
+            y_pred = np.argmax(prediction, axis=1)
+            print("Predicted indices:", y_pred)
+
+            # Convert indices to class labels
             y_pred_labels = [labels[i] for i in y_pred]
+            print("Predicted labels:", y_pred_labels)
 
             return jsonify({
                 "status": {
                     "code": 200,
                     "message": "Prediction successful"
                 },
-            "prediction": y_pred_labels
+                "data": {
+                    "prediction": y_pred_labels
+                }
             })
         except Exception as e:
             return jsonify({
@@ -151,7 +164,6 @@ def predict():
         "data": None
     }), 405
 
-
 if __name__ == "__main__":
-    # app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)), debug=True)
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)), debug=True)
+    # app.run(debug=True)
--- postman.py
+++ postman.py
@@ -10,13 +10,12 @@ data = {
     "A2": 1, "A3": 2, "A4": 3, "A5": 1, "A6": 5, "A8": 3,
     "S1": 3, "S3": 4, "S5": 3, "S6": 4, "S7": 5, "S8": 3,
     "E1": 1, "E3": 2, "E4": 3, "E5": 4, "E7": 5, "E8": 3,
-    "C1": 1,"C2": 1, "C3": 3, "C5": 3, "C7": 5, "C8": 3,
+    "C2": 1, "C3": 3, "C5": 3,"C6":5, "C7": 5, "C8": 3,
     "TIPI1": 1, "TIPI2": 2, "TIPI3": 3, "TIPI4": 4, "TIPI5": 5,
     "TIPI6": 6, "TIPI7": 7, "TIPI8": 4, "TIPI9": 4, "TIPI10": 6,
     "VCL1": 1, "VCL2": 1, "VCL3": 0, "VCL4": 1, "VCL5": 0,
     "VCL6": 1, "VCL10": 0, "VCL11": 1, "VCL12": 1, "VCL13": 1,
-    "VCL14": 1, "VCL15": 1, "education": 1, "gender": 0,
-    "engnat": 0, "religion": 3, "voted": 1
+    "VCL14": 1, "VCL15": 1,
 }
 
 # Send the POST request
